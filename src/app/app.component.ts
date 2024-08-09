caf8afcce7da428533f152555d8c7b1536674932
mauricio-testa <iciotesta@hotmail.com>
2024-08-09 15:35:54 -0300

    test config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-test';
   public htmlContent: String = ''
+  public editorConfig: AngularEditorConfig = {
+    showToolbar: false,
+  }
 }
